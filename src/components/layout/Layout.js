dbc0fe3fdfb8fc45bea29e32a79d370e57a13f5d
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-05-08 02:56:34 +0530

    Adds Google Analytics

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -7,6 +7,16 @@ const Layout = ({ children, props }) => {
   return (
     <>
       <Helmet>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-171526438-1"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'UA-171526438-1');
+        </script>
+
         <title>Someity | Making the Web Accessible for All</title>
       </Helmet>
       <Header props={props} />
