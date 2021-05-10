e838330c8ff3f7862aa470707486267c6cf094f0
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-05-10 21:17:10 +0530

    Adds Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,30 +69,25 @@ module.exports = {
         accessToken: process.env.FORMIUM_ACCESS_TOKEN,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
-    //     trackingIds: [
-    //       "UA-171526438-1", // Google Analytics / GA
-    //     ],
-    //     // This object gets passed directly to the gtag config command
-    //     // This config will be shared across all trackingIds
-    //     gtagConfig: {
-    //       optimize_id: "OPT_CONTAINER_ID",
-    //       anonymize_ip: true,
-    //       cookie_expires: 0,
-    //     },
-    //     // This object is used for configuration specific to this plugin
-    //     pluginConfig: {
-    //       // Puts tracking script in the head instead of the body
-    //       head: false,
-    //       // Setting this parameter is also optional
-    //       respectDNT: true,
-    //       // Avoids sending pageview hits from custom paths
-    //       exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     },
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-171526438-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
+      },
+    },
   ],
 };
